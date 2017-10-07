eb35edaaed145cced3731aabb8b206ba8ac2e15c
Rémi CROS <remi.cros.auditeur@lecnam.net>
2017-10-07 12:06:13 +0200

    vgyeuivbhigr

--- Assets/Scripts/ULCrowdControl.cs
+++ Assets/Scripts/ULCrowdControl.cs
@@ -2,24 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ULCrowdControl : MonoBehaviour {
+public class ULCrowdControl : MonoBehaviour
+{
 
-    private GameObject Player;
-    private Vector3 PreviousPosition;
+    private ULCharacterController Player;
+    private Vector3 lastPosition;
+    public float speed = 1.0f;
+    private Queue<Vector3> previousPositions = new Queue<Vector3>();
+    public float pathfindingGranularity = 0.5f;
+    private Vector3 tmpPosition;
 
-	// Use this for initialization
-	void Start () {
-        Player = GameObject.FindGameObjectsWithTag("Player")[0];
-        PreviousPosition = Player.transform.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-	}
+    // Use this for initialization
+    void Awake()
+    {
+        Player = FindObjectOfType<ULCharacterController>();
+        lastPosition = Player.transform.position;
+        previousPositions.Enqueue(lastPosition);
+        StartCoroutine("CheckPlayerPosition");
+    }
 
-    IEnumerator CheckPlayerPosition(){
-        PreviousPosition = Player.transform.position;
-        yield return new WaitForSeconds(.1f);
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (previousPositions.Count != 1)
+            tmpPosition = previousPositions.Dequeue();
+            transform.Translate (Vector3.Normalize(tmpPosition - transform.position) * speed * Time.fixedDeltaTime);
+    }
+
+    IEnumerator CheckPlayerPosition()
+    {   
+        yield return new WaitForSeconds(pathfindingGranularity);
+        lastPosition = Player.transform.position;
+        previousPositions.Enqueue(lastPosition);
+        Debug.Log("Yolo");
+        StartCoroutine("CheckPlayerPosition");
     }
 }
--- Assets/Scripts/ULPlayerController.cs
+++ Assets/Scripts/ULPlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ULCharacterController : ULCharacter {
+public class ULPlayerController : ULCharacter {
 	public float hugDistance = 1.0f;
 
 	void Start () {
