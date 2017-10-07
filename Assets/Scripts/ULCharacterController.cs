87f2f31174cf46b976960cf06d904b1d3072d02b
joceycesup <thibault.baron56@gmail.com>
2017-10-07 09:08:05 +0200

    Init

---
+++ Assets/Scripts/ULCharacterController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ULCharacterController : MonoBehaviour {
+	public float speed = 4.0f;
+
+	void Start () {
+
+	}
+
+	void FixedUpdate () {
+		Vector3 direction = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical")).normalized;
+		transform.Translate (direction * speed * Time.fixedDeltaTime);
+	}
+}
