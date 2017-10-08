917364baace8d381bb1400f9114f81df236a9ec7
Rémi CROS <remi.cros.auditeur@lecnam.net>
2017-10-08 10:54:56 +0200

    Ya du monde

---
+++ Assets/Scripts/ULCanvasController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ULCanvasController : MonoBehaviour
+{
+
+    private static GameObject buttonHug;
+    private static bool boolbuttonHug = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        buttonHug = GameObject.FindWithTag("buttonHug");
+        buttonHug.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (boolbuttonHug)
+        {
+            if (ULFollowerController.gaiCount != 0)
+            {
+                buttonHug.SetActive(false);
+                boolbuttonHug = false;
+            }
+        }
+    }
+
+    public static void ActiveButtonHug()
+    {
+        if(!boolbuttonHug && ULFollowerController.gaiCount == 0)
+        {
+            boolbuttonHug = true;
+            buttonHug.SetActive(true);
+        }
+    }
+
+}
